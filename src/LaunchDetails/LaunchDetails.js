f6af3f7f90989f07e8203aa9144732a84f48aa0a
jtomassoni <jtomassoni@gmail.com>
2020-10-29 21:47:10 -0600

    launch details-good, needs carousel standards size

--- src/LaunchDetails/LaunchDetails.js
+++ src/LaunchDetails/LaunchDetails.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
-import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Moment from 'react-moment';
+import Carousel from 'react-bootstrap/Carousel';
 
 const LaunchDetails = ({ match }) => {
 	const [launch, setLaunch] = useState('');
@@ -16,84 +23,133 @@ const LaunchDetails = ({ match }) => {
 			.catch((error) => alert('API Fetch Error'));
 		//eslint-disable-next-line
 	}, []);
-	if (!launch) {
-		return null;
+	if (launch.length === 0) {
+		return (
+			<Container
+				style={{
+					display: 'flex',
+					justifyContent: 'space-around',
+					alignItems: 'center',
+					height: '50vh',
+				}}>
+				<div>
+					Loading...
+					<Spinner animation='grow' variant='success' size='sm' />
+				</div>
+			</Container>
+		);
 	}
 
 	return (
-		<div className='detailsContainer'>
-			<div className='detailsPatchImage'>
-				{launch.links.patch.small !== null ? (
-					<img
-						src={launch.links.patch.small}
-						alt={`small ${launch.name} patch`}
-					/>
-				) : (
-					<>
+		<Container>
+			<Row style={{ borderBottom: '10px double black' }}>
+				<Col
+					style={{
+						alignItems: 'center',
+						border: '4px solid gray',
+						textAlign: 'center',
+						margin: '4rem',
+						lineHeight: '5rem',
+					}}>
+					<h3>Mission Name: </h3>
+					<p>{launch.name}</p>
+				</Col>
+				<Col style={{ textAlign: 'center', margin: '1rem' }}>
+					{launch.links.patch.small !== null ? (
 						<img
-							src={process.env.PUBLIC_URL + '/no_image_found.png'}
-							alt={launch.name}
+							src={launch.links.patch.small}
+							alt={`small ${launch.name} patch`}
 						/>
-						<p>There are no patches to display</p>
-					</>
-				)}
-			</div>
-			<div className='detailsMissionInfo'>
-				<h3>Mission Name: {launch.name}</h3>
-				<h3>
-					Mission Date:{' '}
+					) : (
+						<>
+							<img
+								src={process.env.PUBLIC_URL + '/no_image_found.png'}
+								alt={launch.name}
+							/>
+							<p>There are no patches to display</p>
+						</>
+					)}
+				</Col>
+				<Col
+					style={{
+						alignItems: 'center',
+						border: '4px solid gray',
+						textAlign: 'center',
+						margin: '4rem',
+						lineHeight: '3rem',
+					}}>
+					<h3>Mission Date: </h3>{' '}
 					<Moment parse='YYYY-MM-DD HH:mm'> {launch.date_utc}</Moment>
-				</h3>
-			</div>
-			<div className='detailsCardDetails'>
-				<h4>Mission Details:</h4>
-				{launch.details !== null ? (
-					<p>{launch.details}</p>
-				) : (
-					<p>There are no launch details to display.</p>
-				)}
-			</div>
-			<div className='detailsYTPlayer'>
-				{launch.links.youtube_id !== null ? (
-					<ReactPlayer
-						url={`https://www.youtube.com/watch?v=${launch.links.youtube_id}`}
-						controls={true}
-					/>
-				) : (
-					<>
-						<img
-							src={process.env.PUBLIC_URL + '/no_image_found.png'}
-							alt={launch.name}
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					{launch.links.flickr.original.length > 0 ? (
+						<Carousel>
+							{launch.links.flickr.original.map((pic) => {
+								return (
+									<Carousel.Item
+										style={{
+											marginTop: '2rem',
+											maxHeight: '500px',
+											border: '2px solid black',
+										}}
+										key={pic}>
+										<img src={pic} className='img-fluid' alt='launch' />
+									</Carousel.Item>
+								);
+							})}
+						</Carousel>
+					) : (
+						<>
+							<img
+								src={process.env.PUBLIC_URL + '/no_image_found.png'}
+								alt={launch.name}
+							/>
+							<p>There are no photos to display</p>
+						</>
+					)}
+				</Col>
+
+				<Col
+					style={{
+						marginTop: '2rem',
+						border: '2px solid black',
+						textAlign: 'center',
+						lineHeight: '2rem',
+					}}>
+					<h4>Mission Details:</h4>
+					{launch.details !== null ? (
+						<p>{launch.details}</p>
+					) : (
+						<p>There are no launch details to display.</p>
+					)}
+				</Col>
+			</Row>
+			<Row className='justify-content-center'>
+				<Col
+					md='auto'
+					style={{ margin: '5rem', boxShadow: '0 0 20px 50px', padding: 0 }}>
+					{launch.links.youtube_id !== null ? (
+						<ReactPlayer
+							url={`https://www.youtube.com/watch?v=${launch.links.youtube_id}`}
+							controls={true}
 						/>
-						<p>There are no videos to display</p>
-					</>
-				)}
-			</div>
-			<div className='detailsPhotoCarousel'>
-				{launch.links.flickr.original.length > 0 ? (
-					<Carousel>
-						{launch.links.flickr.original.map((pic) => {
-							return (
-								<Carousel.Item key={pic}>
-									<img src={pic} className='img-fluid' alt='launch' />
-								</Carousel.Item>
-							);
-						})}
-					</Carousel>
-				) : (
-					<>
-						<img
-							src={process.env.PUBLIC_URL + '/no_image_found.png'}
-							alt={launch.name}
-						/>
-						<p>There are no photos to display</p>
-					</>
-				)}
-			</div>
+					) : (
+						<>
+							<img
+								src={process.env.PUBLIC_URL + '/no_image_found.png'}
+								alt={launch.name}
+							/>
+							<p>There are no videos to display</p>
+						</>
+					)}
+				</Col>
+			</Row>
 			{/* <div className='detailsVehiclesUsed'>
 				{launch.ships.map(())}
 			</div> */}
-		</div>
+		</Container>
 	);
 };
 
